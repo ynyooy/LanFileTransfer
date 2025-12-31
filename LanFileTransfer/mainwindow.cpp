0ed9567516cce70ce03a5a348fd0f3682ef1fd1f
ynyooy <ynyooy@qq.com>
2025-12-31 19:22:40 +0800

    使用 Model/View 管理任务列表

--- LanFileTransfer/mainwindow.cpp
+++ LanFileTransfer/mainwindow.cpp
@@ -1,34 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStandardItemModel>
+#include <QStandardItem>
 #include <QFileDialog>
+#include <QMessageBox>
 #include <QListView>
-#include <QProgressBar>
-#include <QPushButton>
-#include <QLabel>
-#include <QListWidgetItem>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    // 连接信号与槽
-    connect(ui->receiveButton, &QPushButton::clicked, this, &MainWindow::onReceiveClicked);
-    connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::onSendClicked);
-    connect(ui->settingsButton, &QPushButton::clicked, this, &MainWindow::onSettingsClicked);
+    //调用函数初始化任务列表
+    initializeTaskList();
 
-    connect(ui->selectFileButton, &QPushButton::clicked, this, &MainWindow::onSelectFileClicked);
-    connect(ui->selectFolderButton, &QPushButton::clicked, this, &MainWindow::onSelectFolderClicked);
-    connect(ui->selectTextButton, &QPushButton::clicked, this, &MainWindow::onSelectTextClicked);
+    //连接信号与槽
+    connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::on_sendButton_clicked);
+    connect(ui->receiveButton, &QPushButton::clicked, this, &MainWindow::on_settingsButton_clicked);
+    connect(ui->settingsButton, &QPushButton::clicked, this, &MainWindow::on_receiveButton_clicked);
 
-    // 模拟设备列表
-    QStringList devices = {"设备1", "设备2", "设备3"};
-    ui->deviceListView->addItems(devices);
-
-    // 设置进度条
-    ui->transferProgressBar->setRange(0, 100);
-    ui->transferProgressBar->setValue(0);
 }
 
 MainWindow::~MainWindow()
@@ -36,47 +27,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-// 接收按钮点击事件
-void MainWindow::onReceiveClicked()
+void MainWindow::initializeTaskList()
 {
-    // 接收文件的逻辑
+    //创建任务模型
+    taskModel = new QStandardItemModel;
+
+    //设置模型到QListView
+    ui->fileListView->setModel(taskModel);
+
+    // 添加任务示例
+    QStandardItem *taskItem = new QStandardItem("传输文件1");
+    taskModel->appendRow(taskItem);
 }
 
-// 发送按钮点击事件
-void MainWindow::onSendClicked()
+
+
+
+void MainWindow::on_sendButton_clicked()
 {
-    // 发送文件的逻辑
+
 }
 
-// 设置按钮点击事件
-void MainWindow::onSettingsClicked()
+
+void MainWindow::on_settingsButton_clicked()
 {
-    // 设置逻辑
+
 }
 
-// 选择文件按钮点击事件
-void MainWindow::onSelectFileClicked()
+
+void MainWindow::on_receiveButton_clicked()
 {
-    QString file = QFileDialog::getOpenFileName(this, "选择文件", "", "All Files (*)");
-    if (!file.isEmpty()) {
-        ui->fileListView->addItem(file);  // 添加文件到文件列表
-    }
+
 }
 
-// 选择文件夹按钮点击事件
-void MainWindow::onSelectFolderClicked()
-{
-    QString folder = QFileDialog::getExistingDirectory(this, "选择文件夹");
-    if (!folder.isEmpty()) {
-        ui->folderListView->addItem(folder);  // 添加文件夹到文件夹列表
-    }
-}
-
-// 选择文本按钮点击事件
-void MainWindow::onSelectTextClicked()
-{
-    QString text = QFileDialog::getOpenFileName(this, "选择文本文件", "", "Text Files (*.txt)");
-    if (!text.isEmpty()) {
-        ui->textListView->addItem(text);  // 添加文本文件到文本列表
-    }
-}
--- LanFileTransfer/mainwindow.h
+++ LanFileTransfer/mainwindow.h
@@ -2,9 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QStandardItemModel>
+#include<QStandardItem>
+#include<QListView>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -15,7 +20,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_sendButton_clicked();
+
+    void on_settingsButton_clicked();
+
+    void on_receiveButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    // 添加 QStandardItemModel 成员变量
+    QStandardItemModel *taskModel;  // 用于管理任务列表模型
+
+    // 在构造函数中初始化和更新任务列表模型
+    void initializeTaskList();
 };
 #endif // MAINWINDOW_H
